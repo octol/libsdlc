055457e19173ed80fe24e1bae4de2d4e41ac8a71
Jon Haggblad <jon@haeggblad.com>
2013-03-05 17:36:04 +0000

    New global SDLc.h header file.

---
+++ SDLc.h
@@ -0,0 +1,40 @@
+//    SDLc
+//    Copyright (C) 2000, 2001, 2013 Jon Häggblad
+//
+//    This file is part of SDLc.
+//
+//    SDLc is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    SDLc is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with SDLc.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SDLC_H
+#define SDLC_H
+
+#include "SDLc/BaseSurface.h"
+#include "SDLc/Surface.h"
+#include "SDLc/Screen.h"
+#include "SDLc/Sprite.h"
+
+#include "SDLc/Particle.h"
+
+#include "SDLc/Input.h"
+#include "SDLc/Timer.h"
+
+#include "SDLc/Font.h"
+
+#include "SDLc/Mixer.h"
+#include "SDLc/Music.h"
+#include "SDLc/Sound.h"
+
+#include "SDLc/Misc.h"
+
+#endif // SDLC_H
