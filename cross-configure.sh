beb78ba30178528dbb7e42d8a02d44c82b2364df
Jon Haggblad <jon@haeggblad.com>
2013-05-21 06:50:36 +0100

    Completed the switch to pkg-config checks.

--- cross-configure.sh
+++ cross-configure.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+FLAGS="--enable-static --disable-shared"
 CONFIG_SHELL=/bin/sh
 export CONFIG_SHELL
 #PREFIX=/usr/local/cross-tools
@@ -9,13 +10,6 @@ if [ "$TARGET" = "" ]; then
 fi
 PATH="$PREFIX/bin:$PREFIX/$TARGET/bin:$PATH"
 export PATH
-#if [ -f "$PREFIX/$TARGET/bin/$TARGET-sdl-config" ]; then
-if [ -f "$PREFIX/bin/$TARGET-sdl-config" ]; then
-    #SDL_CONFIG="$PREFIX/$TARGET/bin/$TARGET-sdl-config"
-    SDL_CONFIG="$PREFIX/bin/$TARGET-sdl-config"
-    export SDL_CONFIG
-    echo $SDL_CONFIG
-fi
 cache=cross-config.cache
 if [ -f configure ]; then
     CONFIGURE=configure
@@ -25,9 +19,7 @@ else
     echo "Couldn't find configure - aborting!"
     exit 2
 fi
-sh $CONFIGURE --cache-file="$cache" \
-	--target=$TARGET --host=$TARGET --build=i386-linux \
-	--prefix=$PREFIX/$TARGET --enable-static --disable-shared $*
+sh $CONFIGURE --cache-file="$cache" --host=$TARGET --prefix=$PREFIX/$TARGET $FLAGS $*
 status=$?
 rm -f "$cache"
 exit $status
